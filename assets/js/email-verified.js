b00db39c7fb5d0bb0e434a7ee6689efdce555db4
keisler-au <keisler@live.com>
2025-10-29 14:14:45 +0100

    email verified first trial

--- assets/js/email-verified.js
+++ assets/js/email-verified.js
@@ -1,17 +1,16 @@
 window.addEventListener("DOMContentLoaded", function() {
     const params = new URLSearchParams(window.location.search);
-    const isVerified = params.get("email-verified");
+    const verifiedId = params.get("email-verified");
 
-    if (isVerified && isVerified.toLowerCase() === "true") {
+    if (verifiedId) {
         const modal = document.getElementById("email-verified-modal");
         closeBanner();
         if (modal) modal.style.display = "flex";
-
         const closeBtn = document.getElementById("close-email-verified-modal");
-        if (closeBtn) {
-            closeBtn.addEventListener("click", () => {
-                modal.style.display = "none";
-            });
-        }
+        if (closeBtn) closeBtn.addEventListener("click", () => modal.style.display = "none");
+
+        fetch("https://api.aiforclin.com/email-verified?id=" + verifiedId)
+            .then(res => res.json())
+            .catch(err => console.error(err));
     }
 });
